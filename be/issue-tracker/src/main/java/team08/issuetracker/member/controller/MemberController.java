1468b1c5f25e1ba75964b77b5a59b0a9c4492f94
pjm2571 <pjm2571@naver.com>
2024-05-11 10:57:16 +0900

    feat: CORS 접근을 위한 MemberController CrossOrigin 설정 - @CrossOrigin("*") 설정

--- be/issue-tracker/src/main/java/team08/issuetracker/member/controller/MemberController.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/member/controller/MemberController.java
@@ -3,10 +3,7 @@ package team08.issuetracker.member.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import team08.issuetracker.member.model.dto.MemberCreationDto;
 import team08.issuetracker.member.service.MemberService;
 
@@ -14,6 +11,7 @@ import team08.issuetracker.member.service.MemberService;
 @Slf4j
 @RequestMapping("/member")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class MemberController {
     private final MemberService memberService;
 
