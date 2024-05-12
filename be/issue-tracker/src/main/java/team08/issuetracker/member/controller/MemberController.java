354484d8861662b4ee7296597f519d335dd908f9
pjm2571 <pjm2571@naver.com>
2024-05-13 00:33:26 +0900

    feat: 로그인하여, JWT 토큰을 발급받고 검증하는 기능 구현
    - /member/login으로 POST 요청 시 jwt 토큰 발급
    	- 쿠키에 설정된다.
    
    - /member/validate로 get 요청 시, jwt 토큰 검증
    	- 현재는 sout으로 볼 수만 있게 구현
    	- 추가 구현 필요

--- be/issue-tracker/src/main/java/team08/issuetracker/member/controller/MemberController.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/member/controller/MemberController.java
@@ -1,10 +1,18 @@
 package team08.issuetracker.member.controller;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import team08.issuetracker.jwt.JwtService;
+import team08.issuetracker.member.model.Member;
+import team08.issuetracker.member.model.MemberResponse;
 import team08.issuetracker.member.model.dto.MemberCreationDto;
+import team08.issuetracker.member.model.dto.MemberLoginDto;
 import team08.issuetracker.member.service.MemberService;
 
 @RestController
@@ -14,6 +22,7 @@ import team08.issuetracker.member.service.MemberService;
 @CrossOrigin("*")
 public class MemberController {
     private final MemberService memberService;
+    private final JwtService jwtService;
 
     @PostMapping
     public ResponseEntity<String> registerMember(@RequestBody MemberCreationDto memberCreationDto) {
@@ -21,4 +30,31 @@ public class MemberController {
 
         return ResponseEntity.ok("회원가입 성공!");
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginMember(@RequestBody MemberLoginDto memberLoginDto) {
+        Member member = memberService.loginMember(memberLoginDto);
+
+        MemberResponse response = new MemberResponse(member.getMemberId(), jwtService.createJwtToken(member));
+
+        HttpCookie httpCookie = ResponseCookie.from("jwt-token", response.getToken())
+                .maxAge(7776000)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, httpCookie.toString())
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + response.getToken())
+                .build();
+    }
+
+    @GetMapping("/validate")
+    public ResponseEntity<?> my(@CookieValue(name = "jwt-token") String requestRefreshToken){
+        jwtService.parseJwtToken(requestRefreshToken);
+
+        return ResponseEntity.ok("");
+    }
+
 }
