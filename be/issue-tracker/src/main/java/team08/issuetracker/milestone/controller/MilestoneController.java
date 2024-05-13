456e3479cb1163400757d0db5ddf94876eb2ffb9
Zoonmy <pjm2571@naver.com>
2024-05-13 18:05:48 +0900

    feat: /member/count 로 GET 요청 시, 마일스톤 개수를 리턴하는 기능 구현 - MilestoneCountDto 클래스로 Response Body 리턴 	- totalCount : 전체 마일스톤 수 	- openedCount : 열린 마일스톤 수 	- closedCount : 닫힌 마일스톤 수

--- be/issue-tracker/src/main/java/team08/issuetracker/milestone/controller/MilestoneController.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/milestone/controller/MilestoneController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team08.issuetracker.milestone.model.Milestone;
+import team08.issuetracker.milestone.model.dto.MilestoneCountDto;
 import team08.issuetracker.milestone.model.dto.MilestoneCreationDto;
 import team08.issuetracker.milestone.service.MilestoneService;
 
@@ -22,5 +23,13 @@ public class MilestoneController {
 
         return ResponseEntity.ok("마일스톤 생성 성공! 마일스톤 #" + milestone.getId() + " 이름 : " + milestone.getName());
     }
+
+    @GetMapping("/count")
+    public ResponseEntity<MilestoneCountDto> getMilestoneCount() {
+        MilestoneCountDto milestoneCountDto = milestoneService.getMilestoneCount();
+
+        return ResponseEntity.ok(milestoneCountDto);
+    }
+
 
 }
--- be/issue-tracker/src/main/java/team08/issuetracker/milestone/service/MilestoneService.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/milestone/service/MilestoneService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import team08.issuetracker.exception.milestone.InvalidMilestoneFormException;
 import team08.issuetracker.milestone.model.Milestone;
+import team08.issuetracker.milestone.model.dto.MilestoneCountDto;
 import team08.issuetracker.milestone.model.dto.MilestoneCreationDto;
 import team08.issuetracker.milestone.repository.MilestoneRepository;
 
@@ -22,6 +23,14 @@ public class MilestoneService {
         log.debug("Milestone이 생성되었습니다. Name : {}", milestone.getName());
 
         return milestoneRepository.save(milestone);
+    }
+
+    public MilestoneCountDto getMilestoneCount() {
+        long totalCount = milestoneRepository.count();
+        long openedCount = milestoneRepository.countOpenedMilestones();
+        long closedCount = milestoneRepository.countClosedMilestones();
+
+        return new MilestoneCountDto(totalCount, openedCount, closedCount);
     }
 
     private void validateMilestoneForm(MilestoneCreationDto milestoneCreationDto) {
