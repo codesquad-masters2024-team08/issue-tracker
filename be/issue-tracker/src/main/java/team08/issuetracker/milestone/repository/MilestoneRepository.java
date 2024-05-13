784bf7ae9efddcec213066b979e6f35e2e0484b2
Zoonmy <pjm2571@naver.com>
2024-05-13 18:05:11 +0900

    feat: OpenedMilestone, ClosedMilestone의 개수를 가져오는 Query문 추가 - @Query 어노테이션으로sql문 구현

--- be/issue-tracker/src/main/java/team08/issuetracker/milestone/repository/MilestoneRepository.java
+++ be/issue-tracker/src/main/java/team08/issuetracker/milestone/repository/MilestoneRepository.java
@@ -1,11 +1,17 @@
 package team08.issuetracker.milestone.repository;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import team08.issuetracker.milestone.model.Milestone;
-import team08.issuetracker.support.db.WithInsert;
 
 @Repository
 public interface MilestoneRepository extends CrudRepository<Milestone, Long> {
+
+    @Query("SELECT COUNT(*) FROM milestone WHERE is_open = true")
+    long countOpenedMilestones();
+
+    @Query("SELECT COUNT(*) FROM milestone WHERE is_open = false")
+    long countClosedMilestones();
 
 }
